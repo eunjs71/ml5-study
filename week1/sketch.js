57777637a1d34cd41cbed56d2c42780106d34ac0
Jinsu Eun <eunjs0701@hanmail.net>
2020-01-02 15:02:22 +0900

    week1

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,11 +2,27 @@
 // open up your console - if everything loaded properly you should see 0.3.0
 console.log('ml5 version:', ml5.version);
 
+// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+let classifier;
+// A variable to hold the image we want to classify
+let img;
+
+function preload(){
+	classifier = ml5.imageClassifier('MobileNet');
+  	img = loadImage('images/bird.jpg');
+  	classifier.classify(img, gotResult);
+  	image(img, 0, 0);
+}
+
 function setup(){
-    createCanvas(400, 400);
+    let cnv = createCanvas(400, 400);
+    cnv.parent('sketch_week1')
 }
 
 function draw(){
     background(200);
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(mouseX, mouseY, 50, 50);
     
 }
